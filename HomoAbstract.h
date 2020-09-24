5b1cd6aef5b6935b1ad7ed31dab3f456e79526de
nyildiz <nazim.yildiz@enkoelektronik.com>
2020-09-24 08:45:44 +0300

    Yapilacaklar ve bazi yorumlar eklendi.

--- HomoAbstract.h
+++ HomoAbstract.h
@@ -1,7 +1,22 @@
 #include <stdint.h>
 
 
+/// Her turlu sesi kapsar, maymun, yunus, at, balina vs... bizlerin cikardigi bize gore normal anlasilabilir olanlar
+/// Bu fonksiyon dogal frekansi ve genligi olan sese donusturmeye yapayan bir fonksiyon olabilirdi...
+
+/// Bu islemi yapabilmemiz icin bir donanima ihtiyacimiz var, donanim'da surekli degisebilecegi icin, bir arayuz fonksiyonu
+/// kullanmak dogru bir yaklasim olur.
 typedef char *(*ISound)(char *question);
+
+///     Action() arayuz fonksiyonu eklensin, yemek, film izleme kitap okuma, vs... gibi eylemler icin
+/// yazilacak olan aksiyonlarida STATE-MACHINE kullanarak tasarlayalim. Ornegin yemek yerken lokmanin bitmesini beklemek bu sirada
+/// TV izliyor olmak vs...
+///     Once her bir aksiyon ayri bir fonksiyon olacak sekilde; ornegin ActionYemek(), ActionTV(), ActionYemekTV() gbi...
+///     Bir sonraki asamada eszamanli onceden tanimlanmis aksiyonlarin kombinasyonlarini kullanimini saglaycak bir yapi. Bir kuyruk yapisiyla
+/// eszamanli yapilmasi istenilen aksiyonlarin kuyruga eklenmesi ve bu aksiyonlarin sonlanana kadar devam etmesi ardindan listeden silinmesin.
+/// Her bir aksiyonun farkli state-machine yapisi olabilir. Her bir aksiyon suresi farkli olabilir; film aksiyon 2 saat surecek olsun, filme baslarken
+/// 15dk boyunca bir yandan yemek yeniyor, ardindan da arada bir su iciliyor olabilir... HATTA film izleme aksiyon pause yapilarak calan kapiya ya da telefona
+/// bakilip telefon aksiyon bittikten sonra film resume ile devam ettiriliyor olabilir...
 
 typedef struct{
 
--- main.c
+++ main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "HomoSapiens.h"
+/// Class, Object, Abstraction, Encapsulation, inheritence, polymorphism, Overloading terimlerinin C'deki muadilleri nelerdir?
+///     Abstraction: Detaylari gizle yalnizca gerekli olan fonksiyonlari/metotlari kullanilabilir/gorulebilir yap...
+///     Encapsulation: iliskileri seyleri bir grup altinda toplama yani STRUCT/Class
+/// In Object-Oriented Programming, Encapsulation is defined as binding together the data and the functions that manipulate them.
+///     Polymorphism: A person at the same time can have different characteristic. Like a man at the same time is a father, a husband, an employee.
+/// So the same person posses different behaviour in different situations. This is called polymorphism.
 
+/// Message Passing...
+
+/// Todo overloading olayi getHomoSound() -> getHomoSapiensSound() ornegi ile gosterilecek boylece
+/// getHomoSound(&MehmetObj.HomoBase) yerine getHomoSapiensSound(&MehmetObj) ifadesi kullanilabiliyor olacak
+
+/// Todo ISound() arayuz yapisi ile uzunca if-elseif ya da switch-case yapisi kurmadan ilgili insan turunun farkli sesleri
+/// cikartabilmesini ve ya farkli dilleri konusabilmesini sagliyoruz. Benzer bir yapi HayvanBase sinifindan tureyecek
+/// tum canlilar icin dusunulebilir
+
+/// Todo ilerleyen zamanlar icin ISound() gibi Conversation() arayuz fonksiyonu eklenip NLP kullanilarak yapay zeka tabanli bir
+/// bir chat-botu yada botlari yapilabilir...
+
+/// Todo Insan nesnelerini kullanarak bir chat/konusma uygulamasi yapilabilir... Ali Mehmet'e bir sey soylesin yada sorsun, Mehmet'de
+/// Ali'ye cevap versin gibi...
+
+/// Todo Diger insan turlerininde ISound() arayuz fonksiyonu yazilacak, onlar icin 1 tane olacak sacma-sapan sesler cikartacaklar, Sapiens gibi
+/// bir cok dil opsiyonlari olmayacak
+
+/// Todo setHomoHeight() kullanmak yerine setHomoSapiensHeight() ile cakma-overloading yapabiliriz...
+
+/// Todo C++'da bir cok Creational Pattern mevcut ancak C'de gomulu sistemlerde bu tarz yapilari kullanmamiz mantikli degil, dinamik bellekleme
+/// yontemleri bizim isimize pek gelmiyor(Yinede bazi yaklasimlar bir cok bileseni olan kutuphanelerde static bellek kullanimina dikkat edilerek
+/// uygulanabilir).
 HomoSapiens_t MehmetObj;
 HomoSapiens_t JohnObj;
 //typedef char *(*ISound)(char *question);
@@ -37,6 +66,7 @@ int main()
     printf("\n");
 
     // Mehmet ingilizce ogrendi.
+    /// Bilinen dilleri bir listede tutulup, bir sapiense birden cok dil ozelligi eklenebilir... Veri yapilari ve Algoritmalar...
     printf("MehmetObj ingilizce ogrendi!!!!\n");
     setHomoSapiensLanguage(&MehmetObj, LANGUAGE_ENG);
     printf("MehmetObj.Soru = %s, MehmetObj.Cevap = %s\n", "What's up", getHomoSound(&MehmetObj.HomoBase)("What's up"));
